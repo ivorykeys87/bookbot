eae2f0586fffd24674e5f9b2149a5a578d3eade1
github_ivorykeys87 <chase.r.catalano@gmail.com>
2025-03-04 20:41:51 -0500

    Final product complete

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from stats import get_character_count, get_word_count
+from stats import get_character_count, get_word_count, convert_to_list
+import sys
+
+ 
 
 def get_book_text(file_path):
     with open(file_path) as file:
@@ -6,9 +9,21 @@ def get_book_text(file_path):
     return file_contents
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
-    print(f"{get_word_count(text)} words found in the document")
-    print(get_character_count(text))
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    text = get_book_text(sys.argv[1])
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {sys.argv[1]}...")
+    print("----------- Word Count ----------")
+    print(f"Found",get_word_count(text),"total words")
+    print("--------- Character Count -------")
+    list = convert_to_list(get_character_count(text))
+    for item in list:
+         print(f"{item['character']}: {item['count']}")
+    print("============= END ===============")
+    
 
 
 
--- stats.py
+++ stats.py
@@ -7,9 +7,20 @@ def get_character_count(string):
     each_count={}
     done = []
     for i in new_string:
-        if i not in done:
+        if i not in done and i.isalpha():
             done.append(i)
             count = new_string.count(i)
             each_count[i] = count
     return each_count
 
+def sort_on(dict):
+    return dict["count"]
+
+def convert_to_list(dictionary):
+    new_list=[]
+    for item in dictionary:
+        new_dict = {"character":item, "count":dictionary[item]}
+        new_list.append(new_dict)
+    new_list.sort(reverse=True, key=sort_on)
+    return new_list
+    
